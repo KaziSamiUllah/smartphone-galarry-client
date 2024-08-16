91086dfea5e3ce6ffd80930d9f2f7abee96d045f
KaziSamiUllah <samiullahsagor@gmail.com>
2024-08-16 21:05:25 +0600

    built homepage

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
 const Products = () => {
-  return (
+  const [phones, setPhones] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:5000/phones")
+      .then((res) =>
+
+ res.json())
+      .then((data) => setPhones(data));
+  }, []);
+  
+  console.log(phones);
+    return (
     <div>
+
+
       Product table
       <ProductCard></ProductCard>
     </div>
